62220e43548058cd4c03b95dee70ea2cd03db2a1
Nisarg Shah <48768207+freakNewton@users.noreply.github.com>
2021-01-27 20:21:16 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = '/static/'
+STATIC_URL = '/mysite/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'mysite/static')
